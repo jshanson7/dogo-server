e5f621ef5b23ad9152c332fc4916324de1d63d23
Jeff Hanson <jshanson7@gmail.com>
2015-08-03 14:05:59 -0700

    refactor config.js

--- config.js
+++ config.js
@@ -8,52 +8,14 @@ var env = argv.production ? 'production' :
   'development';
 
 module.exports = {
-  development: {
-    env: 'development',
-    port: 3000,
-    db: {
-      name: 'dogo',
-      client: 'postgresql',
-      host: 'localhost',
-      port: 5432,
-      user: secrets.development.DB_USER,
-      password: secrets.development.DB_PASSWORD
-    }
-  },
-  test: {
-    env: 'test',
-    port: 3000,
-    db: {
-      name: 'dogo',
-      client: 'postgresql',
-      host: 'localhost',
-      port: 5432,
-      user: secrets.test.DB_USER,
-      password: secrets.test.DB_PASSWORD
-    }
-  },
-  staging: {
-    env: 'staging',
-    port: 3000,
-    db: {
-      name: 'dogo',
-      client: 'postgresql',
-      host: 'localhost',
-      port: 5432,
-      user: secrets.staging.DB_USER,
-      password: secrets.staging.DB_PASSWORD
-    }
-  },
-  production: {
-    env: 'production',
-    port: 3000,
-    db: {
-      name: 'dogo',
-      client: 'postgresql',
-      host: 'localhost',
-      port: 5432,
-      user: secrets.production.DB_USER,
-      password: secrets.production.DB_PASSWORD
-    }
+  env: env,
+  port: 3000,
+  db: {
+    name: 'dogo',
+    client: 'postgresql',
+    host: 'localhost',
+    port: 5432,
+    user: secrets[env].DB_USER,
+    password: secrets[env].DB_PASSWORD
   }
-}[env];
+};
