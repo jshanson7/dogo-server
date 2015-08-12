c2b327182dd149a3b4baa07fda80f7ad140e5fa3
Jeff Hanson <jshanson7@gmail.com>
2015-08-11 19:31:21 -0700

    refactoring

--- src/test/routeSpec.js
+++ src/test/routeSpec.js
@@ -8,48 +8,48 @@ const request = () => superagent(app.listen());
 
 describe('Routes', () => {
   
-  describe('GET /api/v1/', () => {
-    it('should return 200', (done) => {
+  describe('GET /api/v1/', () =>
+    it('should return 200', done =>
       request()
         .get('/api/v1/')
-        .expect(200, done);
-    });
-  });
+        .expect(200, done)
+    )
+  );
   
-  describe('GET /api/v1/notfound', () => {
-    it('should return 404', (done) => {
+  describe('GET /api/v1/notfound', () =>
+    it('should return 404', done =>
       request()
         .get('/api/v1/notfound')
-        .expect(404, done);
-    });
-  });
+        .expect(404, done)
+    )
+  );
   
-  restControllers.forEach((restCtl) => {
+  restControllers.forEach(restCtl => {
    
-    describe(`GET /api/v1/${restCtl}`, () => {
-      it('should return 200', (done) => {
+    describe(`GET /api/v1/${restCtl}`, () =>
+      it('should return 200', done =>
         request()
           .get(`/api/v1/${restCtl}`)
           .expect('Content-Type', /json/)
-          .expect(200, done);
-      });
-    });
+          .expect(200, done)
+      )
+    );
     
-    describe(`GET /api/v1/${restCtl}/badrequest`, () => {
-      it('should return 400', (done) => {
+    describe(`GET /api/v1/${restCtl}/badrequest`, () =>
+      it('should return 400', done =>
         request()
           .get(`/api/v1/${restCtl}/badrequest`)
-          .expect(400, done);
-      });
-    });
+          .expect(400, done)
+      )
+    );
     
-    describe(`GET /api/v1/${restCtl}/-1`, () => {
-      it('should return 404', (done) => {
+    describe(`GET /api/v1/${restCtl}/-1`, () =>
+      it('should return 404', done =>
         request()
           .get(`/api/v1/${restCtl}/-1`)
-          .expect(404, done);
-      });
-    });
+          .expect(404, done)
+      )
+    );
   
   });
 });
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -3,16 +3,14 @@
 import VError from 'verror';
 import revalidator from 'revalidator';
 
-export default (object, schema, options) => {
-  return new Promise((resolve, reject) => {
-    const response = revalidator.validate(object, schema, options);
-    return response.valid ?
-      resolve() :
-      reject(
-        new VError(
-          'validation errors: \n\t%s',
-          response.errors.map(e => e.property + ': ' + e.message).join('\n\t')
-        )
-      );
-  });
-};
+export default (object, schema, options) => new Promise((resolve, reject) => {
+  const response = revalidator.validate(object, schema, options);
+  return response.valid ?
+    resolve() :
+    reject(
+      new VError(
+        'validation errors: \n\t%s',
+        response.errors.map(e => e.property + ': ' + e.message).join('\n\t')
+      )
+    );
+});
