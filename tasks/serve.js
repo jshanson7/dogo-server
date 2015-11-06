eb28f35f63fcf0104225e52419a5d6c6243a1a61
Jeff Hanson <jshanson7@gmail.com>
2015-11-06 13:14:05 -0800

    add forever for production

--- tasks/serve.js
+++ tasks/serve.js
@@ -4,7 +4,6 @@ import nodemon from 'gulp-nodemon';
 
 const app = resolve(__dirname, '../src');
 
-gulp.task('serve', ['node']);
+gulp.task('serve', ['nodemon']);
 gulp.task('serve:watch', ['nodemon']);
-gulp.task('node', () => require(app));
 gulp.task('nodemon', () => nodemon({ exec: 'node', script: app, watch: app }));
