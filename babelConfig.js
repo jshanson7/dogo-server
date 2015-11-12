f6cea96d3ac9f34219e03501882e0682d8059661
Jeff Hanson <jshanson7@gmail.com>
2015-11-11 17:42:59 -0800

    switch to babel-resolver

--- babelConfig.js
+++ babelConfig.js
@@ -1,8 +1,7 @@
 var resolve = require('path').resolve;
 var localModules = resolve(__dirname, 'src');
-var moduleResolver = require('./lib/utils/moduleResolver')(localModules);
 
 module.exports = {
   stage: 0,
-  resolveModuleSource: moduleResolver
+  resolveModuleSource: require('babel-resolver')(localModules)
 };
