1157e2f857f569ecc14b40f505544ecaaa17f51f
Jeff Hanson <jshanson7@gmail.com>
2015-11-17 19:55:08 -0800

    stub bookshelf methods on app

--- src/models/App.js
+++ src/models/App.js
@@ -1,3 +1,17 @@
 export default class App {
   static Name = 'App';
+
+  fetch() {
+    return this;
+  }
+
+  fetchAll() {
+    const col = [this];
+    col.toJSON = () => col;
+    return col;
+  }
+
+  toJSON() {
+    return this;
+  }
 }
